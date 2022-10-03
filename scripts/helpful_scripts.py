d570a182ca52866a37453f492c95c862ca51ba5c
dennis-mureti <dennismureti47@gmail.com>
2022-10-03 23:42:04 +0300

    key hasg verify

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -81,4 +81,6 @@ def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
     mock_price_feed = MockV3Aggregator.deploy(
         decimals, initial_value, {"from": account}
     )
+    link_token = LinkToken.deploy({"from":account})
+    VRFCoordinatorMock.deploy(link_token.address, {"from": account})
     print("Deployed!!")
