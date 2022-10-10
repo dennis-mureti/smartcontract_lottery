6a140a521af1874e3707844708785a7374f80896
dennis-mureti <dennismureti47@gmail.com>
2022-10-10 23:13:50 +0300

    unit test_cant_enter_unless_starter

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -1,15 +1,15 @@
 # on testing this function we expect to get 0.019
 # or in nwei 190000000000000000
-from brownie import Lottery, accounts, config, network
+from brownie import Lottery, accounts, config, network,exceptions
 from web3 import Web3
 from scripts.deploy_lottery import deploy_lottery
-from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
 import pytest
 
 
 def test_get_entrance_fee():
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
-        pytest.skip
+        pytest.skip()
     #Arrange 
     lottery = deploy_lottery() #this will give us our lottery contract
     # act
@@ -17,3 +17,12 @@ def test_get_entrance_fee():
     entrance_fee = lottery.getEntranceFee()
     # assert
     assert expected_entrance_fee == entrance_fee
+
+def test_cant_enter_unless_started():
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    # act/assert
+    with pytest.raises(exceptions.VirtualmachineError):
+        lottery.enter({"from": get_account(), "value": lottery.getEntranceFee()})
